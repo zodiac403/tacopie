761e6d25acadc9c6347f28cb08a907160c9b69a4
zodiac403 <zodiac403@gmx.de>
2017-10-27 14:58:29 +0200

    Rename macro for Unix configuration.

--- sources/network/common/tcp_socket.cpp
+++ sources/network/common/tcp_socket.cpp
@@ -47,7 +47,7 @@
 #define __TACOPIE_LENGTH(size) static_cast<int>(size)       // for Windows, convert buffer size to `int`
 #pragma warning ( disable: 4996 )                           // `inet_ntoa` is deprecated as it does not support IPv6
 #else
-#define LENGTH(size) size                                   // for Unix, keep buffer size as `size_t`
+#define __TACOPIE_LENGTH(size) size                                   // for Unix, keep buffer size as `size_t`
 #endif /* _WIN32 */
 
 namespace tacopie {
